c278ad752711aa9ffef8dd7b95d9942eb01c6856
abelsesabo-prog <abelsesabo@gmail.com>
2026-08-07 12:18:02 +0300

    update app.js

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const config = require("./config/config");
 const { connectMongoDB } = require("./database/mongo");
-
+const routes = require("./routes");
 const app = express();
 
 app.use(express.json());
