7d4bdb22a6d3f519d37a06f888fd6345c1c45a67
abelsesabo-prog <abelsesabo@gmail.com>
2026-08-07 13:40:23 +0300

    update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+import express from "express";
 
 const router = express.Router();
 
@@ -9,4 +9,4 @@ router.get("/health", (req, res) => {
     });
 });
 
-module.exports = router;
+export default router;
