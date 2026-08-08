ec71592e1f2909ead2271b37b480e5e78e737359
abelsesabo-prog <abelsesabo@gmail.com>
2026-08-08 15:08:17 +0300

    update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { healthCheck } from "../controllers/healthController.js";
+import productRoutes from "./productRoutes.js";
 
 const router = express.Router();
 
 router.get("/health", healthCheck);
+
+router.use("/products", productRoutes);
 
 export default router;
