6ed78fcb2479cfd9a937a641b05e4198fa03cd75
abelsesabo-prog <abelsesabo@gmail.com>
2026-09-02 11:08:27 +0300

    security: expose authentication endpoint

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { healthCheck } from "../controllers/healthController.js";
 import productRoutes from "./productRoutes.js";
+import authRoutes from "./authRoutes.js";
 
 const router = express.Router();
 
 router.get("/health", healthCheck);
+router.use("/auth", authRoutes);
 router.use("/products", productRoutes);
 
 export default router;
